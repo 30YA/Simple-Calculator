f4064cad122aa9c2cfef821596748a4fefb32dab
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-05 16:00:15 +0330

    fix bug v1.2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 "use strict";
 function addtodis(value) {
-    document.getElementById("display").value += value;
+    if (value == "0" && document.getElementById("display").value == "0") {
+        document.getElementById("display").value == "0";
+    }
+    else{
+        if ((value == "." || value == "+" || value == "-" || value == "×" || value == "÷" || value == "^") && document.getElementById("display").value == "") {
+            document.getElementById("display").value = "";
+        }
+        else{
+            document.getElementById("display").value += value;
+        }
+    }
 };
 function final() {
     if (document.getElementById("display").value != "") {
@@ -9,13 +19,18 @@ function final() {
         document.getElementById("display").value = (document.getElementById("display").value).replaceAll("×","*");
         document.getElementById("display").value = eval(document.getElementById("display").value)
     }
+    if (document.getElementById("display").value == "Infinity") {
+        document.getElementById("display").value = "not'work";
+    }
 };
 function delett() {
     document.getElementById("display").value = "";
 }
-function mathCal() {
-    const val = document.getElementById("display").value;
-    document.getElementById("display").value = Math.sqrt(val);
+function mathCal(value) {
+    if (document.getElementById("display").value != "") {
+        const val = document.getElementById("display").value;
+        document.getElementById("display").value = Math[value](val);
+    }
 }
 // function tavann(value) {
 //     document.getElementById("display").value += value;
